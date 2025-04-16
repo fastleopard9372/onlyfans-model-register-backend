3b6d762ee5592919a445e8fde03aba6d4fe7dfc8
fastleopard9372 <fastleopard9372@gmail.com>
2025-04-16 23:48:09 +0900

    fix

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,7 +6,6 @@ const emailService = require('../services/emailService');
 const fs = require('fs').promises;
 const path = require('path');
 const generateCode = require('../utils/generateCode');
-const emailService = require('../services/emailService');
 
 const addModel = async (req, res, next) => {
   try {
